65a4aa442b4ca1b0ae93ed9636d2ab3d1096558d
Ignas Bačius <ignas.bacius@gmail.com>
2018-03-22 22:27:20 +0200

    Add script to build deb package

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # olimp-control
 Remote machine access via HTTPS (pull model)
 
-To install, drop the `.py` files into `/usr/local/bin` and install the systemd unit, save the shared key as `/etc/olimp-control/key`, making sure it is mode `400` and owned by `root:root`.
+To run `olimp-control`, save the shared key as `/etc/olimp-control/key`, making sure it is mode `400` and owned by `root:root`.
---
+++ makedeb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required packages: build-essential, debhelper, devscripts
+#
+# For more info on building deb packages, see:
+#   https://www.debian.org/doc/manuals/maint-guide/
+#   https://wiki.debian.org/Packaging
+
+ver='1.0'
+projdir="olimp-control-${ver}"
+projtar="olimp-control_${ver}.orig.tar.gz"
+builddir="build"
+
+mkdir "${builddir}"
+git archive --format=tar.gz --prefix="${projdir}/" --output="${builddir}/${projtar}" HEAD
+cd "${builddir}"
+tar -xzf "${projtar}"
+cd "${projdir}/debian"
+debuild -A -us -uc
